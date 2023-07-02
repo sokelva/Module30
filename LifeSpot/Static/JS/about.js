9244a761c643e3a27869616e3980ca5bee549596
sokelva <sokelva@mail.ru>
2023-07-02 12:42:49 +0300

    Добавлили счетчик Like-ов.

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -26,16 +26,55 @@ function getReview() {
     writeReview(review)
 }
 
+
+
 /*
-* Запишем отзыв на страницу
+ * Запишем объект на страницу
+ *
+ * */
+const writeReview = review => {
+    let likeCounter = '';
+
+    // Если публикуется отзыв - добавляем ему кнопку с лайками.
+    if (review.hasOwnProperty('rate')) {
+
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, и само значение счётчика отделяем пробелом
+        // Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+    // Запишем результат 
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-    text">\n' + `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` + `<p>${review['text']}</p>` + '</div>';
+}
+
+
+/*
+* Увеличивает счётчик лайков
 *
 * */
-const writeReview = review => {
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>` +
-        '</div>';
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
 }
+
+
 
 /*
 * Конструктор, через который создаётся комментарий
@@ -55,10 +94,11 @@ function Comment() {
         this.empty = true
         return
     }
-
     // Сохраним текущее время
     this.date = new Date().toLocaleString()
 }
+
+
 
 /*
 * Оставить комментарий
